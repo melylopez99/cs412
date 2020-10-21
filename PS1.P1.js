cf6b40c4e711aa718e0baf1d693a697880a1fe5e
melylopez99 <mlopez99@bu.edu>
2020-10-21 15:19:39 -0400

    Starting fresh for PS3

--- PS1.P1.js
+++
@@ -1,5 +0,0 @@
-// function that takes a string as its input and returns a new string that contains all of the
-// letters in the original string, but in reverse alphabetical order.
-const reverse = string => string.split('').sort().reverse().join('');
-module.exports = {reverse};
-
