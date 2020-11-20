2d545e9bde71b8497908e0b2a95388d6288febe7
melylopez99 <mlopez99@bu.edu>
2020-11-20 13:56:21 -0500

    Finished assignment

--- PS5/routes/ps4.js
+++ PS5/routes/ps4.js
@@ -3,20 +3,48 @@ const router = express.Router();
 const fetch = require('node-fetch')
 const FETCHCONFIG = require('../config/fetchConfig')
 
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const getAsync = promisify(client.get).bind(client);
+const existsAsync = promisify(client.exists).bind(client);
+const setAsync = promisify(client.set).bind(client);
+const expireAsync = promisify(client.expire).bind(client);
+
+client.flushdb((err, success) => {
+    if (err) {
+        throw new Error(err)
+    }
+});
+
 router.route('/')
-    .post((req,res,next) => {
-    let githubInfo = doRequest(req.body.gname)
-        .then(cleanReturnValue => {
-            res.render('ps4',{'name': req.body.gname, 'num_repo': cleanReturnValue.public_repos,
-            'git_url': cleanReturnValue.html_url});
-        });
-    })
-
-const doRequest = async value => {
-    let rawReturnValue = await fetch(FETCHCONFIG.fetchOptions.url + value);
-    let cleanReturnValue = await rawReturnValue.json();
-    return cleanReturnValue;
-
-};
+    .post(async (req,res,next) => {
+        // get the Github username
+        let gname = req.body.gname;
+        console.log(gname);
+        // check cache
+        let match = await existsAsync(gname);
+        if (match) {
+            let value = await getAsync(gname);
+            let response = {
+                githubName: gname,
+                repos: value,
+                cached: true
+            }
+            res.send(response);
+        } else {
+            let githubInfo = await fetch(FETCHCONFIG.fetchOptions.url + gname);
+            let githubInfoJson = await githubInfo.json();
+            let status = await setAsync(gname, githubInfoJson.public_repos);
+            status = await expireAsync(gname, 15)
+            let response = {
+                githubName: gname,
+                repos: githubInfoJson.public_repos,
+                cached: false
+            }
+            res.send(response)
+        }
+    });
 
 module.exports = router;
